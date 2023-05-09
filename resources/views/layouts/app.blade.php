73b046e7b4cea18cb3ea6f18140edf38a16e48b3
sjaboss <sjaboss@gmail.com>
2023-05-09 20:06:33 -0300

    headero

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,16 +21,16 @@
         <x-banner />
 
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @livewire('navigation-menu')
+            @livewire('navigation')
 
             <!-- Page Heading -->
-            @if (isset($header))
+     {{--        @if (isset($header))
                 <header class="bg-white dark:bg-gray-800 shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
-            @endif
+            @endif --}}
 
             <!-- Page Content -->
             <main>
