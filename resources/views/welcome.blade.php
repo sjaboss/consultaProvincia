7274e80457dd413f239b0ee03b27c1b3c781c17f
sjaboss <sjaboss@gmail.com>
2023-05-12 10:27:15 -0300

    sjaboss1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <div>
         <a href="/"
         class=" flex flex-col items-center justify-center  text-white cursor-pointer font-semiboldsemi h-full">
-        <img src="{{ asset('img/logosjaboss.png') }}" class=" max-w-screen-sm object-cover ">
+        <img src="{{ asset('img/logosjaboss.png') }}" class=" w-full object-cover p-6 ">
     </a>
     </div>
     <footer>
